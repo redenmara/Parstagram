7d1b7a6ea2e5193b85fcbf1480ac70fb6d4d50e7
Okwuolisa Umeokolo <umeokol1@msu.edu>
2021-03-14 16:00:03 -0400

    Query Posts

---
+++ Parstagram/PostTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostTableViewCell.swift
+//  Parstagram
+//
+//  Created by Okwuolisa Umeokolo on 3/14/21.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
