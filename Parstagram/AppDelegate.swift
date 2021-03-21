b04841367c95e1de8c55c059903a48ef247e534c
Okwuolisa Umeokolo <umeokol1@msu.edu>
2021-03-21 17:07:48 -0400

    Staying Logged In

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -11,7 +11,7 @@ import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -21,6 +21,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
               $0.server = "https://parseapi.back4app.com"
           }
         Parse.initialize(with: parseConfig)
+        
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+        }
         return true
     }
 
